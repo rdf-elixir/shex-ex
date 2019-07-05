fc0addaaebbf1d2d294c235b8434cfda3847e22b
Marcel Otto <marcelotto@gmx.de>
2019-07-05 16:25:28 +0200

    Various ShEx test suite improvements
    - test equality of ShExC and ShExJ versions of the schema in validation
      tests
    - speed up test runs by extracting the test bodies which are basically
      macros into separate non-macro functions

--- test/shex_test_suite_test.exs
+++ test/shex_test_suite_test.exs
@@ -17,24 +17,29 @@ defmodule ShEx.TestSuiteTest do
 
       @tag test_case: test_case
       test TestSuite.test_case_title(test_case), %{test_case: test_case} do
-        assert {:ok, shexj_schema} =
-                 test_case
-                 |> ShEx.TestSuite.test_case_file(:json)
-                 |> ShEx.TestSuite.file()
-                 |> File.read!()
-                 |> ShEx.ShExJ.Decoder.decode()
-
-        assert {:ok, shexc_schema} =
-                 test_case
-                 |> ShEx.TestSuite.test_case_file(:shex)
-                 |> ShEx.TestSuite.file()
-                 |> File.read!()
-                 |> ShEx.ShExC.Decoder.decode()
-
-        assert shexc_schema == shexj_schema
+        schema_test(test_case)
       end
     end)
+
+    defp schema_test(test_case) do
+      assert {:ok, shexj_schema} =
+               test_case
+               |> ShEx.TestSuite.test_case_file(:json)
+               |> ShEx.TestSuite.file()
+               |> File.read!()
+               |> ShEx.ShExJ.Decoder.decode()
+
+      assert {:ok, shexc_schema} =
+               test_case
+               |> ShEx.TestSuite.test_case_file(:shex)
+               |> ShEx.TestSuite.file()
+               |> File.read!()
+               |> ShEx.ShExC.Decoder.decode()
+
+      assert shexc_schema == shexj_schema
+    end
   end
+
 
   describe "negativeSyntax" do
     # These tests violate the ShEx2 grammar
@@ -42,14 +47,18 @@ defmodule ShEx.TestSuiteTest do
     |> Enum.each(fn test_case ->
       @tag test_case: test_case
       test TestSuite.test_case_title(test_case), %{test_case: test_case} do
-        assert {:error, _} =
-                 test_case
-                 |> ShEx.TestSuite.test_case_file(:shex)
-                 |> ShEx.TestSuite.file()
-                 |> File.read!()
-                 |> ShEx.ShExC.Decoder.decode()
+        negative_syntax_test(test_case)
       end
     end)
+
+    defp negative_syntax_test(test_case) do
+      assert {:error, _} =
+               test_case
+               |> ShEx.TestSuite.test_case_file(:shex)
+               |> ShEx.TestSuite.file()
+               |> File.read!()
+               |> ShEx.ShExC.Decoder.decode()
+    end
   end
 
 
@@ -85,18 +94,19 @@ defmodule ShEx.TestSuiteTest do
         "node_kind_example",
         "recursion_example",
         "dependent_shape",
-        "1dot_fail-empty-err",
-        "2RefS1-IS2",
-        "2RefS1-Icirc",
-        "2RefS2-IS1",
-        "3circRefS1-IS23",
-        "3circRefS1-IS2-IS3",
-        "3circRefS3",
-        "3circRefS3-IS12",
-        "3circRefS123",
-        "3circRefS123-Icirc",
-        "3circRefS1-Icirc",
-        "3circRefS1-IS2-IS3-IS3",
+
+        # unknown result format (.val)
+#        "2RefS1-IS2",
+#        "2RefS1-Icirc",
+#        "2RefS2-IS1",
+#        "3circRefS1-IS23",
+#        "3circRefS1-IS2-IS3",
+#        "3circRefS3",
+#        "3circRefS3-IS12",
+#        "3circRefS123",
+#        "3circRefS123-Icirc",
+#        "3circRefS1-Icirc",
+#        "3circRefS1-IS2-IS3-IS3",
       ]
       |> Enum.each(fn test_subject ->
         if test_case |> TestSuite.test_case_name() |> String.starts_with?(test_subject),
@@ -105,6 +115,17 @@ defmodule ShEx.TestSuiteTest do
 
 #      [
 #        "3circRefS123-Icirc",
+#        "2RefS1-IS2",
+#        "2RefS1-Icirc",
+#        "2RefS2-IS1",
+#        "3circRefS1-IS23",
+#        "3circRefS1-IS2-IS3",
+#        "3circRefS3",
+#        "3circRefS3-IS12",
+#        "3circRefS123",
+#        "3circRefS123-Icirc",
+#        "3circRefS1-Icirc",
+#        "3circRefS1-IS2-IS3-IS3",
 #      ]
       if RDF.iri(SHT.Import) in TestSuite.test_case_traits(test_case),
         do: @tag skip: "TODO: imports"
@@ -163,66 +184,90 @@ defmodule ShEx.TestSuiteTest do
 
       @tag test_case: test_case
       test TestSuite.test_case_title(test_case), %{test_case: test_case} do
-        test_case_action =
-          validation_manifest()
-          |> ShEx.TestSuite.test_case_action(test_case)
-
-        # TODO: Test this also with the schema from the corresponding ShExJ file if present
-        assert {:ok, schema} =
-                 test_case_action
-                 |> RDF.Description.first(SHT.schema)
-                 |> ShEx.TestSuite.file()
-                 |> File.read!()
-                 |> ShEx.ShExC.Decoder.decode(base: @validation_base_iri)
-
-        assert {:ok, graph} =
-                 test_case_action
-                 |> RDF.Description.first(SHT.data)
-                 |> ShEx.TestSuite.file()
-                 |> RDF.Turtle.read_file(base: @validation_base_iri)
-
-        shape_map_file =
-          test_case_action
-          |> RDF.Description.first(SHT.map)
-          |> ShEx.TestSuite.file()
-
-        map =
-          if shape_map_file do
-            # TODO: decode query ShapeMap from JSON
-          else
-            shape =
-              if RDF.iri(SHT.Start) in TestSuite.test_case_traits(test_case),
-                do: :start,
-                else: RDF.Description.first(test_case_action, SHT.shape)
-            ShEx.ShapeMap.new(%{
-              RDF.Description.first(test_case_action, SHT.focus) => shape})
-          end
-
-        # TODO: decode result ShapeMap from JSON
-        result = nil
-
-        # TODO: sht:shapeExterns <../schemas/shapeExtern.shextern>
-        shape_externs =
-          test_case_action
-          |> RDF.Description.first(SHT.shapeExterns)
-          |> ShEx.TestSuite.file()
-
-        external_shape =
-          if shape_externs do
-            assert {:ok, shape_externs_schema} =
-                     shape_externs
-                     |> File.read!()
-                     |> ShEx.ShExC.Decoder.decode()
-          end
-
-        test_case
-        |> ShEx.TestSuite.test_case_type()
-        |> to_string()
-        |> String.trim_leading("http://www.w3.org/ns/shacl/test-suite#")
-        |> test_validation(graph, schema, map, result, external_shape)
+        validation_test(test_case)
       end
     end)
 
+    defp validation_test(test_case) do
+      test_case_action =
+        validation_manifest()
+        |> ShEx.TestSuite.test_case_action(test_case)
+
+      schema_file =
+        test_case_action
+        |> RDF.Description.first(SHT.schema)
+        |> ShEx.TestSuite.file()
+
+      assert {:ok, schema} =
+               schema_file
+               |> File.read!()
+               |> ShEx.ShExC.Decoder.decode(base: @validation_base_iri)
+
+      shexj_schema_file = String.replace_trailing(schema_file, "shex", "json")
+      if File.exists?(shexj_schema_file) do
+        assert {:ok, shexj_schema} =
+                 shexj_schema_file
+                 |> File.read!()
+                 |> ShEx.ShExJ.Decoder.decode(base: @validation_base_iri)
+
+        # TODO: The schemas for these seem to be different ... see also "schemas" tests
+        unless test_case
+               |> TestSuite.test_case_name()
+               |> String.starts_with?("FocusIRI2groupBnodeNested2groupIRIRef") do
+          assert shexj_schema == schema
+        end
+      end
+
+      assert {:ok, graph} =
+               test_case_action
+               |> RDF.Description.first(SHT.data)
+               |> ShEx.TestSuite.file()
+               |> RDF.Turtle.read_file(base: @validation_base_iri)
+
+      shape_map_file =
+        test_case_action
+        |> RDF.Description.first(SHT.map)
+        |> ShEx.TestSuite.file()
+
+      map =
+        if shape_map_file do
+          assert {:ok, shape_map} =
+                   shape_map_file
+                   |> File.read!()
+                   |> ShEx.ShapeMap.JSON.decode()
+          shape_map
+        else
+          shape =
+            if RDF.iri(SHT.Start) in TestSuite.test_case_traits(test_case),
+               do: :start,
+               else: RDF.Description.first(test_case_action, SHT.shape)
+          ShEx.ShapeMap.new(%{
+            RDF.Description.first(test_case_action, SHT.focus) => shape})
+        end
+
+      # TODO: decode result ShapeMap from JSON
+      result = nil
+
+      # TODO: sht:shapeExterns <../schemas/shapeExtern.shextern>
+      shape_externs =
+        test_case_action
+        |> RDF.Description.first(SHT.shapeExterns)
+        |> ShEx.TestSuite.file()
+
+      external_shape =
+        if shape_externs do
+          assert {:ok, shape_externs_schema} =
+                   shape_externs
+                   |> File.read!()
+                   |> ShEx.ShExC.Decoder.decode()
+        end
+
+      test_case
+      |> ShEx.TestSuite.test_case_type()
+      |> to_string()
+      |> String.trim_leading("http://www.w3.org/ns/shacl/test-suite#")
+      |> test_validation(graph, schema, map, result, external_shape)
+    end
 
     defp test_validation(type, graph, schema, shape_map, nil, _external_shape) do
       assert %ShEx.ShapeMap{} = result = ShEx.validate(graph, schema, shape_map)
