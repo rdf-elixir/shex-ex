6be61ef31772b56aa37843ac6d2a530e285d933c
Marcel Otto <marcelotto@gmx.de>
2019-07-15 01:57:53 +0200

    Add project metadata

--- mix.exs
+++ mix.exs
@@ -1,14 +1,50 @@
 defmodule ShEx.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/marcelotto/shex-ex"
+
+  @version File.read!("VERSION") |> String.trim
+
   def project do
     [
       app: :shex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
+
+      # Hex
+      package: package(),
+      description: description(),
+
+      # Docs
+      name: "ShEx.ex",
+      docs: [
+        main: "ShEx",
+        source_url: @repo_url,
+        source_ref: "v#{@version}",
+        extras: ["CHANGELOG.md"],
+      ]
+    ]
+  end
+
+  defp description do
+    """
+    An implementation of ShEx for Elixir.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Marcel Otto"],
+      licenses: ["MIT"],
+      links: %{
+        "Homepage" => "https://rdf-elixir.dev",
+        "GitHub" => @repo_url,
+        "Changelog" => @repo_url <> "/blob/master/CHANGELOG.md",
+      },
+      files: ~w[lib src/*.xrl src/*.yrl mix.exs VERSION *.md]
     ]
   end
 
